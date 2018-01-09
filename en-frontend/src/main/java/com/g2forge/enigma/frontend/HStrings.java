1ac199a71e58852d8f7250e0fd254f1d2aa217d2
Greg Gibeling <gdgib@outlook.com>
2018-01-08 20:51:47 -0800

    G2-78 Created HString.unescape

--- en-frontend/src/main/java/com/g2forge/enigma/frontend/HStrings.java
+++ en-frontend/src/main/java/com/g2forge/enigma/frontend/HStrings.java
@@ -1,6 +1,6 @@
 package com.g2forge.enigma.frontend;
 
-import com.g2forge.alexandria.java.core.error.NotYetImplementedError;
+import com.g2forge.alexandria.java.core.helpers.HString;
 import com.g2forge.alexandria.java.marker.Helpers;
 
 import lombok.AllArgsConstructor;
@@ -25,36 +25,10 @@ public class HStrings {
 		}
 	}
 
-	public static String unescape(String string) throws NotYetImplementedError {
-		final StringBuilder retVal = new StringBuilder(string.length());
-		for (int i = 0; i < string.length(); i++) {
-			final char current = string.charAt(i);
-			if (current == '\\') {
-				switch (string.charAt(i + 1)) {
-					case '“':
-					case '”':
-					case '\\':
-						retVal.append(string.charAt(i + 1));
-						break;
-					case 'n':
-						retVal.append('\n');
-						break;
-					case 'r':
-						retVal.append('\r');
-						break;
-					default:
-						throw new NotYetImplementedError();
-				}
-				i++;
-			} else retVal.append(current);
-		}
-		return retVal.toString();
-	}
-
 	public static String unwrap(String string) {
 		if (string.length() < 2) throw new IllegalArgumentException("String \"" + string + "\" is not long enough to be wrapped in quotes!");
 		for (Quotes quotes : Quotes.values()) {
-			if (quotes.is(string)) return unescape(string.substring(1, string.length() - 1));
+			if (quotes.is(string)) return HString.unescape(string.substring(1, string.length() - 1));
 		}
 		throw new IllegalArgumentException("String \"" + string + "\" is not wrapped in matching quotes!");
 	}
