e68cabc509df4b9df5a688981062d70c73ba1b02
Greg Gibeling <gdgib@outlook.com>
2019-01-10 16:03:34 -0800

    Implemented additional HTML classes

---
+++ en-web/src/main/java/com/g2forge/enigma/web/html/html/Anchor.java
@@ -0,0 +1,29 @@
+package com.g2forge.enigma.web.html.html;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import com.g2forge.enigma.web.html.convert.HTMLField;
+import com.g2forge.enigma.web.html.convert.HTMLTag;
+import com.g2forge.enigma.web.html.convert.IReflectiveHTMLElement;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Singular;
+
+@Data
+@Builder
+@AllArgsConstructor
+@HTMLTag("a")
+public class Anchor implements IBodyElement, IReflectiveHTMLElement {
+	protected final String href;
+
+	@HTMLField(property = false)
+	@Singular
+	protected final Collection<?> elements;
+
+	public Anchor(String href, Object... elements) {
+		this(href, Arrays.asList(elements));
+	}
+}
