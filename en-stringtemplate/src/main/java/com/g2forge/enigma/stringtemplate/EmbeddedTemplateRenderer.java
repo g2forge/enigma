fe4fb6a9462539934a92ff32205167c29e9f634e
Greg Gibeling <gdgib@outlook.com>
2019-10-11 17:26:35 -0700

    G2-618 Synchronize template cache mappings

--- en-stringtemplate/src/main/java/com/g2forge/enigma/stringtemplate/EmbeddedTemplateRenderer.java
+++ en-stringtemplate/src/main/java/com/g2forge/enigma/stringtemplate/EmbeddedTemplateRenderer.java
@@ -4,12 +4,26 @@ import java.util.function.Function;
 
 import com.g2forge.alexandria.adt.associative.cache.Cache;
 import com.g2forge.alexandria.adt.associative.cache.LRUCacheEvictionPolicy;
+import com.g2forge.alexandria.java.function.IFunction1;
 import com.g2forge.alexandria.reflection.record.v2.reflection.ReflectedRecordType;
+
+import lombok.AccessLevel;
+import lombok.Getter;
 
 public class EmbeddedTemplateRenderer {
 	public final static EmbeddedTemplateRenderer DEFAULT = new EmbeddedTemplateRenderer();
 
 	public final static EmbeddedTemplateRenderer STRING = new EmbeddedTemplateRenderer("\n");
+
+	@Getter(value = AccessLevel.PROTECTED, lazy = true)
+	private static final IFunction1<? super Class<?>, ? extends ReflectedRecordType> recordFunction = new IFunction1<Class<?>, ReflectedRecordType>() {
+		protected final IFunction1<? super Class<?>, ? extends ReflectedRecordType> cache = new Cache<>(ReflectedRecordType::new, new LRUCacheEvictionPolicy<>(30));
+
+		@Override
+		public synchronized ReflectedRecordType apply(Class<?> argument) {
+			return cache.apply(argument);
+		}
+	};
 
 	public static String toString(Object object) {
 		return DEFAULT.render(object);
@@ -22,7 +36,7 @@ public class EmbeddedTemplateRenderer {
 	}
 
 	protected EmbeddedTemplateRenderer(char delimiterStartChar, char delimiterStopChar, String lineSeparator, Function<? super Object, ? extends Object> adapter) {
-		this.group = new STGroupJava("UTF-8", delimiterStartChar, delimiterStopChar, lineSeparator, adapter, new Cache<>(ReflectedRecordType::new, new LRUCacheEvictionPolicy<>(30)));
+		this.group = new STGroupJava("UTF-8", delimiterStartChar, delimiterStopChar, lineSeparator, adapter, getRecordFunction());
 	}
 
 	public EmbeddedTemplateRenderer(String lineSeparator) {
