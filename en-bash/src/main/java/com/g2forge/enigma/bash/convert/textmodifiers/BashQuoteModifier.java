88b3ae0fd53ee613fca2b42e029403c00dbcf0a4
Greg Gibeling <gdgib@outlook.com>
2020-03-14 19:02:12 -0700

    G2-671 Add quote control to BashString

--- en-bash/src/main/java/com/g2forge/enigma/bash/convert/textmodifiers/BashQuoteModifier.java
+++ en-bash/src/main/java/com/g2forge/enigma/bash/convert/textmodifiers/BashQuoteModifier.java
@@ -5,31 +5,32 @@ import java.util.List;
 
 import com.g2forge.alexandria.java.core.error.NotYetImplementedError;
 import com.g2forge.alexandria.java.core.helpers.HCollection;
-import com.g2forge.alexandria.java.core.marker.ISingleton;
 import com.g2forge.alexandria.java.function.IFunction1;
 import com.g2forge.alexandria.java.text.TextUpdate;
-import com.g2forge.alexandria.java.text.escape.BashEscapeType;
-import com.g2forge.alexandria.java.text.quote.HQuote;
+import com.g2forge.alexandria.java.text.quote.BashQuoteType;
 import com.g2forge.enigma.backend.model.modifier.ITextModifier;
 
-public class BashDoubleExpandQuoteModifier implements ITextModifier, ISingleton {
-	protected static final BashDoubleExpandQuoteModifier INSTANCE = new BashDoubleExpandQuoteModifier();
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
-	public static BashDoubleExpandQuoteModifier create() {
-		return INSTANCE;
-	}
+@Data
+@Builder(toBuilder = true)
+@RequiredArgsConstructor
+public class BashQuoteModifier implements ITextModifier {
+	protected final BashQuoteType quoteType;
 
 	@Override
 	public List<? extends List<? extends TextUpdate<?>>> computeUpdates(List<CharSequence> list) {
 		final List<List<TextUpdate<?>>> retVal = new ArrayList<>(list.size());
 		for (int i = 0; i < list.size(); i++) {
 			final List<TextUpdate<?>> updates = new ArrayList<>();
-			if (i == 0) updates.add(new TextUpdate<>(0, 0, IFunction1.create(HQuote.QUOTE_DOUBLE)));
-			BashEscapeType.DoubleExpand.getEscaper().computeEscape(list.get(i).toString(), update -> {
+			if (i == 0) updates.add(new TextUpdate<>(0, 0, IFunction1.create(quoteType.getPrefix())));
+			quoteType.getEscapeType().getEscaper().computeEscape(list.get(i).toString(), update -> {
 				if (update.getFunction() instanceof IFunction1.Identity) return;
 				updates.add(update);
 			});
-			if (i == list.size() - 1) updates.add(new TextUpdate<>(HCollection.getLast(list).length(), 0, IFunction1.create(HQuote.QUOTE_DOUBLE)));
+			if (i == list.size() - 1) updates.add(new TextUpdate<>(HCollection.getLast(list).length(), 0, IFunction1.create(quoteType.getPostfix())));
 			retVal.add(updates.isEmpty() ? null : updates);
 		}
 		return retVal;
--- en-bash/src/main/java/com/g2forge/enigma/bash/model/expression/BashString.java
+++ en-bash/src/main/java/com/g2forge/enigma/bash/model/expression/BashString.java
@@ -3,6 +3,8 @@ package com.g2forge.enigma.bash.model.expression;
 import java.util.List;
 
 import com.g2forge.alexandria.java.core.helpers.HCollection;
+import com.g2forge.alexandria.java.text.quote.BashQuoteType;
+import com.g2forge.alexandria.java.text.quote.QuoteControl;
 
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +15,18 @@ import lombok.Singular;
 @Builder(toBuilder = true)
 @RequiredArgsConstructor
 public class BashString implements IBashExpression {
+	protected final BashQuoteType quoteType;
+
+	protected final QuoteControl quoteControl;
+
 	@Singular
 	protected final List<Object> elements;
 
+	public BashString(BashQuoteType quoteType, QuoteControl quoteControl, Object... elements) {
+		this(quoteType, quoteControl, HCollection.asList(elements));
+	}
+
 	public BashString(Object... elements) {
-		this(HCollection.asList(elements));
+		this(BashQuoteType.BashDoubleExpand, QuoteControl.IfNeeded, HCollection.asList(elements));
 	}
 }
