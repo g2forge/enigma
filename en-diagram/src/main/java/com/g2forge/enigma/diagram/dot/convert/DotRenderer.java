c2691a1c0b12806d8d85e6ccf4bf8b2ada825186
Greg Gibeling <gdgib@outlook.com>
2020-06-07 07:19:25 -0700

    G2-834 Fix merge conflict

--- en-diagram/src/main/java/com/g2forge/enigma/diagram/dot/convert/DotRenderer.java
+++ en-diagram/src/main/java/com/g2forge/enigma/diagram/dot/convert/DotRenderer.java
@@ -12,6 +12,7 @@ import com.g2forge.enigma.backend.convert.ARenderer;
 import com.g2forge.enigma.backend.convert.IExplicitRenderable;
 import com.g2forge.enigma.backend.convert.IRendering;
 import com.g2forge.enigma.backend.convert.textual.ATextualRenderer;
+import com.g2forge.enigma.backend.text.model.modifier.TextNestedModified.TextNestedModifiedBuilder;
 import com.g2forge.enigma.diagram.dot.model.DotAttribute;
 import com.g2forge.enigma.diagram.dot.model.DotEdge;
 import com.g2forge.enigma.diagram.dot.model.DotGraph;
@@ -29,6 +30,10 @@ import lombok.RequiredArgsConstructor;
 public class DotRenderer extends ATextualRenderer<Object, IDotRenderContext> {
 	protected class DotRenderContext extends ARenderContext implements IDotRenderContext {
 		protected Boolean directed = null;
+
+		public DotRenderContext(TextNestedModifiedBuilder builder) {
+			super(builder);
+		}
 
 		@Override
 		protected IDotRenderContext getThis() {
@@ -50,6 +55,20 @@ public class DotRenderer extends ATextualRenderer<Object, IDotRenderContext> {
 	}
 
 	protected static class DotRendering extends ARenderer.ARendering<Object, IDotRenderContext, IExplicitRenderable<? super IDotRenderContext>> {
+		protected void attributes(IDotAttributed e, IDotRenderContext c) {
+			final List<IDotAttribute> attributes = e.getAttributes();
+			if ((attributes != null) && !attributes.isEmpty()) {
+				c.append(" [");
+				boolean first = true;
+				for (IDotAttribute attribute : attributes) {
+					if (first) first = false;
+					else c.append(", ");
+					c.render(attribute, IDotAttribute.class);
+				}
+				c.append(']');
+			}
+		}
+
 		@Override
 		protected void extend(TypeSwitch1.FunctionBuilder<Object, IExplicitRenderable<? super IDotRenderContext>> builder) {
 			builder.add(IExplicitDotRenderable.class, e -> c -> e.render(c));
@@ -96,28 +115,14 @@ public class DotRenderer extends ATextualRenderer<Object, IDotRenderContext> {
 				c.append(e.getName()).append('=').append(quoted);
 			});
 		}
-
-		protected void attributes(IDotAttributed e, IDotRenderContext c) {
-			final List<IDotAttribute> attributes = e.getAttributes();
-			if ((attributes != null) && !attributes.isEmpty()) {
-				c.append(" [");
-				boolean first = true;
-				for (IDotAttribute attribute : attributes) {
-					if (first) first = false;
-					else c.append(", ");
-					c.render(attribute, IDotAttribute.class);
-				}
-				c.append(']');
-			}
-		}
 	}
 
 	@Getter(lazy = true, value = AccessLevel.PROTECTED)
 	private static final IRendering<Object, IDotRenderContext, IExplicitRenderable<? super IDotRenderContext>> renderingStatic = new DotRendering();
 
 	@Override
-	protected DotRenderContext createContext() {
-		return new DotRenderContext();
+	protected IDotRenderContext createContext(TextNestedModifiedBuilder builder) {
+		return new DotRenderContext(builder);
 	}
 
 	@Override
